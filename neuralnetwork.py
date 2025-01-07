ad176bec08588ca36f3c813758ddf5cfd7cb9e38
StijnCuppers <160853699+StijnCuppers@users.noreply.github.com>
2025-01-07 14:44:18 +0100

    neural network file setup

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -1,8 +1,25 @@
-def model_self():
+# Imports 
+import torch
+
+# Dummy dataset(replace when preprocessing is done)
+
+# Models
+def model_self(): # Stijn, implement LSTM model here
     return None
 
-def model_pretrained():
+def model_pretrained(): # Max, implement pretrained model here
     return None
 
-def model_fourier():
+def model_fourier(): # Jan-Paul, implement Fourier model here
     return None
+
+def train_model(model = 'Self'):
+    if model == 'Self':
+        model = model_self()
+    elif model == 'Pretrained':
+        model = model_pretrained()
+    else:
+        model = model_fourier()
+    return None
+
+# evaluation block
