fa094ecdf7579cf592962258de686eb4cfe08cd3
MiroBruinsma <m.bruinsma@student.tudelft.nl>
2025-01-12 14:17:04 +0100

    Get_bubbles()

--- preprocess.py
+++ preprocess.py
@@ -7,7 +7,7 @@ import torch
 import Evtx.Evtx as evtx
 
 #Folder containing .bin, .binlog, (.evt, .evtlog) of one run
-input_folder = R"INPUT_PATH_HERE"
+input_folder = R"C:\Users\TUDelft\Desktop\bubble_data"
 
 
 def find_files(folder_path):
